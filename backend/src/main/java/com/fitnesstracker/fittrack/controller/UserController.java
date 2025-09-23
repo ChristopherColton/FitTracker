a6688b7e976a79db699f3d3d0e1334306b3aa5f0
ChristopherColton <cjcolton@asu.edu>
2025-09-23 14:47:39 -0700

    finished UserController

--- backend/src/main/java/com/fitnesstracker/fittrack/controller/UserController.java
+++ backend/src/main/java/com/fitnesstracker/fittrack/controller/UserController.java
@@ -8,5 +8,24 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping
 public class UserController{
-    //private final UserRep repo;
+    private final UserRep repo;
+
+    public UserController(UserRep repo){
+        this.repo = repo;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return repo.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        return repo.save(user);
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id){
+        return repo.findById(id).orElse(null);
+    }   
 }
