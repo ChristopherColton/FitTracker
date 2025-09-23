d68287c143036c5fa1c902659d2cfdb4c9517a1a
ChristopherColton <cjcolton@asu.edu>
2025-09-23 11:10:40 -0700

    adding repo and controller

---
+++ backend/src/main/java/com/fitnesstracker/fittrack/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.fitnesstracker.fittrack.controller;
+
+import com.fitnesstracker.fittrack.user.User;
+import com.fitnesstracker.fittrack.rep.UserRep;
+import java.util.List;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+public class UserController{
+    //private final UserRep repo;
+}
---
+++ backend/src/main/java/com/fitnesstracker/fittrack/rep/WorkoutRep.java
@@ -0,0 +1,9 @@
+package com.fitnesstracker.fittrack.rep;
+
+import com.fitnesstracker.fittrack.user.Workout;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface WorkoutRep extends JpaRepository<Workout, Long>{
+    List<Workout> findByUserId(Long userId);
+}
